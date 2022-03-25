c68fe6abbd278251f0037399dc342afc02de27d9
syahirahhalim <101188428+syahirahhalim@users.noreply.github.com>
2022-03-25 16:23:17 +0800

    Update PurchaseOrderApplicationTests.java

--- src/test/java/iss/nus/edu/sg/PurchaseOrder/PurchaseOrderApplicationTests.java
+++ src/test/java/iss/nus/edu/sg/PurchaseOrder/PurchaseOrderApplicationTests.java
@@ -1,6 +1,5 @@
 package iss.nus.edu.sg.PurchaseOrder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
